9971d49837b36f2ee5296f7bdc50f4bf60c1a4b4
Alex <rubanovv.alex@gmail.com>
2023-03-02 17:31:33 +0100

    test of redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+
+import reducer from './reducer';
+import { inc, dec, rnd} from './actions';
+
+
+
+const store = createStore(reducer);
+const { dispatch, subscribe, getState } = store;
+
+const update = () => {
+    document.getElementById('counter').textContent = getState().value;
+}
+
+subscribe(update);
+
+const bindActionCreator = (creator, dispatch) => (...args) => {
+    dispatch(creator(...args));
+}
+
+const incDispatch = bindActionCreator(inc, dispatch);
+const decDispatch = bindActionCreator(dec, dispatch);
+const rndDispatch = bindActionCreator(rnd, dispatch);
+
+
+
+document.getElementById('inc').addEventListener('click', incDispatch);
+
+document.getElementById('dec').addEventListener('click', decDispatch);
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    rndDispatch(value);
+});
+
+
+// let state = reducer(undefined, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// console.log(state);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+
+    </>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
